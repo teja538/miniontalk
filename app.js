771b64d97ad47b72ea00c6878494d3e2832f764a
teja538 <saisree538@gmail.com>
2021-06-04 16:28:26 +0530

    input output proccessing done

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //reference to the query selector is stored in variable at 38:min watch video
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var txtOutput = document.querySelector("#txt-output")
 
 btnTranslate.addEventListener("click",function clickEventHandler()
 {
-    console.log("you clicked the button "+txtInput.value)
+    txtOutput.innerText = "translated text is : " + txtInput.value  
 })
+
