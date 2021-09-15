fd58b04d92e96c7773ae8cae7165997a46d070d9
teja538 <saisree538@gmail.com>
2021-09-15 12:03:35 +0530

    basic translate done

--- app.js
+++ app.js
@@ -3,8 +3,33 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var txtOutput = document.querySelector("#txt-output")
 
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text)
+{
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(err)
+{
+    console.log("error occured ",err);
+    alert("something wrong with server!!!Try again after some time");
+}
+
 btnTranslate.addEventListener("click",function clickEventHandler()
 {
-    txtOutput.innerText = "translated text is : " + txtInput.value  
+    //txtOutput.innerText = "translated text is : " + txtInput.value  
+    var inputText = txtInput.value; //taking input
+
+    //calling server for processing
+    fetch(getTranslationURL(inputText))
+    .then(res => res.json())
+    .then(json => {
+        var translateTxt = json.contents.translated;
+        txtOutput.innerText = translateTxt;
+        
+    })
+    .catch(errorHandler)
 })
 
